1acf925b62ddcea7415fe7ff8ef4a74a7735cc96
egosum277-glitch <egosum277@gmail.com>
2026-04-25 10:41:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,15 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Я працюю 24/7 🚀")
+
+async def normal_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f"Ти написав: {update.message.text}")
+
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
@@ -6,7 +18,6 @@ def main():
 
     print("Бот запущений...")
     app.run_polling()
-
 
 if __name__ == "__main__":
     main()
